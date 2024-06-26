0285e785d4ca5887ad799dc24078910be769d95a
LvYuanWang <1175430934@qq.com>
2024-06-26 08:40:31 +0800

    笔记模板

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log(3 + [1]);
+
+const obj = {};
+console.log(obj.info);
+
+function divide(a) {
+    return a / 2;
+}
+
+const b1 = divide("hello");
+console.log(b1);
+
+let user;
+console.log(user.name);
---
+++ src/index.ts
@@ -0,0 +1,15 @@
+/* 在JS中一些无效的代码运行 */
+// console.log(3 + [1]);
+
+// const obj = {};
+// console.log(obj.info);
+
+// function divide(a) {
+//     return a / 2;
+// }
+
+// const b1 = divide("hello");
+// console.log(b1);
+
+// let user;
+// console.log(user.name);
