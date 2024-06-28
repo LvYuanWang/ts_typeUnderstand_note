2df6f55c08ef328f5f2ec4d16b9ffeabb7cf20f2
LvYuanWang <1175430934@qq.com>
2024-06-28 08:28:51 +0800

    feat: 添加结构化类型的代码示例和说明

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
 /* 结构化类型 */
+
+type Person = {
+    name: string,
+    age: number
+}
+
+type Animal = {
+    name: string,
+    age: number
+}
+
+const person: Person = {
+    name: 'John',
+    age: 21
+}
+
+// const animal: Animal = {
+//     name: 'dog',
+//     age: 2
+// }
+
+const animal: Animal = person;
