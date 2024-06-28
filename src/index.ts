602259bf2475d55ce2cd0ead065343f9573ce441
LvYuanWang <1175430934@qq.com>
2024-06-28 10:02:33 +0800

    feat: 添加动物的爱好属性 为Animal类型添加了一个可选的hobby属性，以便记录动物的爱好。

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,8 @@ type Person = {
 
 type Animal = {
     name: string,
-    age: number
+    age: number,
+    hobby?: string
 }
 
 const person: Person = {
@@ -21,3 +22,36 @@ const person: Person = {
 // }
 
 const animal: Animal = person;
+const animal2: Animal = {
+    name: 'dog',
+    age: 2,
+    hobby: 'eat'
+}
+
+function greet(person: Person) {
+    console.log(`Hello, ${person.name}`);
+}
+
+greet(animal2); // 不会报错
+
+
+class User {
+    firstName: string;
+    lastName: string;
+    age: number;
+
+    constructor(firstName: string, lastName: string, age: number) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+}
+
+class People {
+    constructor(public firstName: string, public lastName: string, public age: number) { }
+}
+
+let a = new User('John', 'Doe', 21);
+let b = new People('Straw', 'Berry', 20);
+
+a = b; // 不会报错： People 类型的实例可以赋值给 User 类型的实例(因为 People 类型的实例包含了 User 类型的所有属性)
